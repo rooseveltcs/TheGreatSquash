bf1c89e311f0dff06f7981e669e0479d82b2f81d
ros_dmlamarca <ros_dmlamarca@WROSRM31922.seattleschools.org>
2014-09-29 09:24:53 -0700

    TestClient recieves messages from TestServer

--- The Great Squash/src/LAN/TestClient.java
+++ The Great Squash/src/LAN/TestClient.java
@@ -1,5 +1,20 @@
 package LAN;
 
-public class TestClient {
+import java.io.DataInputStream;
+import java.net.Socket;
 
+public class TestClient {
+	static Socket socket;
+	static DataInputStream in;
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Connecting...");
+		socket = new Socket("10.135.66.52",7777);
+		System.out.println("Connection successful");
+		in = new DataInputStream(socket.getInputStream());
+		while(true) {
+			String test = in.readUTF();
+			System.out.println("Message from server: " + test);
+		}
+	}
 }
