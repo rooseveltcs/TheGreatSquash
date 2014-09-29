327835491d69d9fe5f9977cee4a438b6dd1c9820
UltGuardian <alj@hushmail.me>
2014-09-29 09:24:48 -0700

    Server messaging system done

--- The Great Squash/src/LAN/TestServer.java
+++ The Great Squash/src/LAN/TestServer.java
@@ -1,5 +1,25 @@
 package LAN;
 
-public class TestServer {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class TestServer {
+    static ServerSocket serverSocket;
+    static Socket socket;
+    static DataOutputStream out;
+    
+    public static void main(String[] args) throws IOException{
+        System.out.println("Starting server...");
+        serverSocket = new ServerSocket(7777);
+        System.out.println("ServerStarted...");
+        socket = serverSocket.accept();
+        out = new DataOutputStream(socket.getOutputStream());
+        Scanner console = new Scanner(System.in);
+        while(true){
+            out.writeUTF(console.nextLine());
+        }
+    }
 }
