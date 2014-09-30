e6be7ec0ac11d9bd5ff0c58a37ff186ef676a7ce
UltGuardian <alj@hushmail.me>
2014-09-30 14:07:27 -0700

    Trying to fix the error, so far unsuccessful. THough I know when the error occurs

--- The Great Squash/src/LAN/TestClient.java
+++ The Great Squash/src/LAN/TestClient.java
@@ -16,7 +16,7 @@ public class TestClient {
         System.out.println("Connecting to server...");
         //Aaron's work station is 10.135.66.52
         //Dylan's work station is 10.135.65.230
-        socket = new Socket("10.135.66.52",4180);
+        socket = new Socket("10.135.66.52", 4180);
         System.out.println("Connection successful.");
         in = new DataInputStream(socket.getInputStream());
         out = new DataOutputStream(socket.getOutputStream());
@@ -24,11 +24,11 @@ public class TestClient {
         Thread thread = new Thread(streamIn);
         thread.start();
         Scanner sc = new Scanner(System.in);
-        while(!false){
+        while (!false) {
             String outMessage = sc.nextLine();
             out.writeUTF(outMessage);
         }
-        
+
     }
 }
 
@@ -43,7 +43,7 @@ class Input implements Runnable {
     public void run() {
         while (!false) {
             try {
-                String message = STREAM_IN.readUTF();
+                String message = STREAM_IN.readUTF();//the error occurs here
                 System.out.println(message);
             } catch (IOException ex) {
                 ex.printStackTrace();
