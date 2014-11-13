5fc1eaeddc06a9fb430b6214c57d95fc66ea2f09
UltGuardian <alj@hushmail.me>
2014-11-13 09:37:24 -0800

    Made the test classes for the broadcast stuff

---
+++ The Great Squash/src/LAN/TestBroadcastClient.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LAN;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ros_aljacobson001
+ */
+public class TestBroadcastClient implements Runnable{
+    private DataInputStream STREAM_IN;
+    private DataOutputStream STREAM_OUT;
+    private Socket BROADCAST_SOCKET;
+    public TestBroadcastClient(){
+        try {
+            BROADCAST_SOCKET = new Socket("255.255.255.255",CommandHolder.BROADCAST_PORT_NUMBER);
+        } catch (UnknownHostException ex) {
+            System.out.println("TestBroadcastClient: Can't connect to the broadcast address");
+        } catch (IOException ex) {
+            System.out.println("TestBroadcastClient: Can't connect to the broadcast port");
+        }
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try {
+                String in = STREAM_IN.readUTF();
+                interpretIn(in);
+            } catch (IOException ex) {
+                System.out.println("TestBroadcastClient: Cant read from the stream in");
+            }
+        }
+    }
+    
+    public void interpretIn(String in){
+        Scanner inScanner = new Scanner(in);
+        String theCommand = inScanner.next();
+        if(theCommand.equals(CommandHolder.SERVER_BROADCAST_MESSAGE)){
+            System.out.println("TestBroadcastClient: Message from the server: " + in);
+        }
+    }
+}
---
+++ The Great Squash/src/Main/tests/TestBroadcast.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main.tests;
+
+import LAN.TestBroadcastClient;
+import LAN.TestBroadcastServer;
+
+/**
+ *
+ * @author ros_aljacobson001
+ */
+public class TestBroadcast {
+    public static final boolean shouldServer = true;
+    public static TestBroadcastServer SERVER;
+    public static TestBroadcastClient CLIENT;
+    public static void main(String[] args){
+        if(shouldServer){
+            SERVER = new TestBroadcastServer();
+        }
+        CLIENT = new TestBroadcastClient();
+        Thread clientThread = new Thread(CLIENT);
+        clientThread.run();
+    }
+}
